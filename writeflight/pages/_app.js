df80c0c3210a675f9ab5cdecda63ccedf94a2e8e
Kiefer Luttrell <kieferluttrell@gmail.com>
2023-11-21 13:38:00 -0600

    cleanup setup

---
+++ writeflight/pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'; // Path to your global CSS file
+
+function WriteFlightApp({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+}
+
+export default WriteFlightApp;
+
